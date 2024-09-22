da3dc2edebb0b417d734104b3f70bc260b6fbfd2
GeniousSonu <sahinurislamm2002@gmail.com>
2024-09-23 00:28:24 +0530

    Testing

---
+++ Fuel.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Fuel {
+
+    public static void Main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the no of Liters to fill the Tank: ");
+    }
+}
